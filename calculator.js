d6c011206d7f92fb1c688b3151cac380c2cbfa28
Bryce Chen <mac8@129mac18.valleychristian.net>
2017-01-10 14:48:00 -0800

    added css

--- calculator.js
+++ calculator.js
@@ -10,12 +10,12 @@ function v(val) {
 	document.getElementById("d").value += val;
 }
 //Evaluate the equation
-function  e() {
+function e() {
 	try {
 		d(eval(document.getElementById("d")value));
-}
-catch(err) {
-	d("Error");
-}
+	}
+	catch(err) {
+		d("Error");
+	}
 }
 
